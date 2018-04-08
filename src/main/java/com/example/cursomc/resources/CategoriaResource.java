3ccaa8dd73aab9ce3f79e26c96af00dfbdd9287b
Gregory Gregio <gregorygregio@hotmail.com>
2018-04-08 19:41:41 -0300

    Banco de dados H2 e mapeamento da entidade de categoria

--- src/main/java/com/example/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/example/cursomc/resources/CategoriaResource.java
@@ -3,6 +3,7 @@ package com.example.cursomc.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,8 @@ import com.example.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
+
+
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
