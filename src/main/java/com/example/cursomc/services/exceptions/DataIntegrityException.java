716798d74a83b4756165e00b67afaa73b396e32b
Gregory Gregio <gregorygregio@hotmail.com>
2018-05-01 22:17:06 -0300

    Deletando categorias via delete

---
+++ src/main/java/com/example/cursomc/services/exceptions/DataIntegrityException.java
@@ -0,0 +1,19 @@
+package com.example.cursomc.services.exceptions;
+
+public class DataIntegrityException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public DataIntegrityException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DataIntegrityException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+}
